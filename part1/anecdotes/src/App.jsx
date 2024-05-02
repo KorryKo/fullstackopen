834131cf5a032cb00a75769118ee46d0d6bbfa47
Marina <maryaleks@mail.ru>
2024-05-02 10:54:15 -0400

    add solutions for tasks 1.12, 1.13, 1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length));
+
+  const getTheMostVotedAnecdoteIndex = () =>
+    points.indexOf(Math.max(...points));
+
+  const setNextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const setVotes = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p> {anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <div>
+        <button onClick={setVotes}>vote</button>
+        <button onClick={setNextAnecdote}>next anecdote</button>
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[getTheMostVotedAnecdoteIndex()]}</p>
+    </div>
+  );
+};
+
+export default App;
